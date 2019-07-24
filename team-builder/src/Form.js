e0fa298e494ad1d3c8ad71228a8d8f313fbe7cb0
Red <vhanchar@ucsc.edu>
2019-07-24 13:34:53 -0700

    built inputs for name, email, and role

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -3,8 +3,42 @@ import React, {useState} from 'react';
 const Form = () => {
     console.log('the form was called');
     return (
-        <div className='Form'> 
-        Form
+        <div className='form'> 
+            {/* input for name */}
+            <div className='form-field'>
+                <label htmlFor="nameInput"> Name: </label>
+                <input
+                type='text'
+                className='form-input'
+                name='name'
+                placeholder='Enter your name'
+                />
+            </div>
+
+            {/* input for email */}
+           <div className='form-field'>
+                <label htmlFor="emailInput"> Email: </label>
+                <input
+                type='email'
+                className='form-input'
+                name='email'
+                placeholder='Enter your email'
+                />
+            </div>
+            
+            {/* input for role */}
+            <div className='form-field'>
+                <label htmlFor="roleInput"> Role: </label>
+                <input
+                type='text'
+                className='form-input'
+                name='role'
+                placeholder='Enter your role'
+                />
+            </div>
+
+
+
         </div>
     )
     
